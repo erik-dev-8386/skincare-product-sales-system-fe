064ffab32e9b83f05c6531591a5a17de8c8c4213
NguyenNgocMinh-SE182009 <nguyenngocminh17012004@gmail.com>
2025-01-24 23:22:16 +0700

    minh

--- src/component/BlogCard/BlogCard.jsx
+++ src/component/BlogCard/BlogCard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Card } from "antd";
+import "./BlogCard.css"
 
 const BlogCard = ({ title, description, image }) => {
   return (
-    <Card
+    <Card className="card-blog"
       hoverable
-      style={{ width: 300, margin: "10px" }}
-      cover={<img alt={title} src={image} />}
+      style={{ width: 300, height: 350, margin: "10px" }}
+      cover={<img className="img-blog" alt={title} src={image} />}
     >
       <Card.Meta title={title} description={description} />
     </Card>
