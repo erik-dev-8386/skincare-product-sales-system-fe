a5984a1deecab03fdfb948c3b5090f67d98fa2e4
NguyenNgocMinh-SE182009 <nguyenngocminh17012004@gmail.com>
2025-02-09 22:06:03 +0700

    minh 11:05 9/2/2025

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,17 @@ import DiscountDetail from './pages/users/discount/DiscountDetail.jsx';
 import AboutUs from './pages/users/aboutUs/AboutUs.jsx';
 import BlogDetail from "./pages/users/blog/BlogDetail.jsx";
 import Shopping from './pages/users/shoppingCart/Shopping.jsx'
-import Cart from './pages/users/cartt/Cart.jsx'
+import Cart from './pages/users/cart/Cart.jsx'
 import Kho from './pages/users/listskincare/Kho.jsx'
+
 function App() {
+
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/auth/callback" element={<Callback />} />
+      {/* <Route path="/auth/callback" element={<Callback />} /> */}
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/terms-of-use" element={<TermsOfUse />} />
       <Route path="/blog" element={<Blog />} />
